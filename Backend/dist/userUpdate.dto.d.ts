5d1ecb89ac698c4b7fee79a5ac5df895e86e53b8
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-01-06 13:39:32 +0300

    Initial trial

---
+++ Backend/dist/userUpdate.dto.d.ts
@@ -0,0 +1,3 @@
+export declare class UserUpdateDto {
+    username: string;
+}
---
+++ Backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserSchema } from './user.models';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://ruthalemfanta:ggCjAMfGGIggO0FU@cluster0.4sdzlj8.mongodb.net/?retryWrites=true&w=majority'),
+    MongooseModule.forFeature([{name:'user', schema:UserSchema}])
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
