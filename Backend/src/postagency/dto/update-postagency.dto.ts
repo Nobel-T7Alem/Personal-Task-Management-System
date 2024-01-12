c9fee8dd1b7e1a9704127653fe5a0b2b0bb38395
JoshuaTaye <tayejoshua4@gmail.com>
2024-01-12 23:57:07 +0300

    Merge

--- Backend/src/postagency/dto/update-postagency.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostagencyDto } from './create-postagency.dto';
-
-export class UpdatePostagencyDto extends PartialType(CreatePostagencyDto) {}
--- Backend/src/volunteer/dto/volunteer-login.dto.ts
+++
@@ -1,4 +0,0 @@
-export class VolunteerLoginDto {
-    username: string;
-    password: string;
-  }
